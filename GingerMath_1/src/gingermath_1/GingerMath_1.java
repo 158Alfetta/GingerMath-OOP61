f045518bc0dea26533909891d1b2eb0bcb004f83
158Alfetta <32899555+158Alfetta@users.noreply.github.com>
2018-11-16 10:59:54 +0700

    update properly

--- GingerMath_1/src/gingermath_1/GingerMath_1.java
+++ GingerMath_1/src/gingermath_1/GingerMath_1.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package testingui;
+package gingermath_1;
 
 /**
  *
  * @author Corvette
  */
-public class TestingUI {
+public class GingerMath_1 {
 
     /**
      * @param args the command line arguments
